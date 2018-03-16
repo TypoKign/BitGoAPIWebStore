765db63f66880fa44ff4b0329f45998cd7d8e2b1
Typo Kign <typokign1@gmail.com>
2018-03-16 16:09:09 -0700

    Removed handlebars b/c of syntax conflict with Angular

--- server.js
+++ server.js
@@ -2,23 +2,15 @@ const express = require("express");
 const port = 80;
 
 const path = require("path")
-const handlebars = require("express-handlebars")
 
 const app = express();
 
-app.engine('.hbs', handlebars({
-    defaultLayout: 'main',
-    extname: '.hbs',
-    layoutsDir: path.join(__dirname, 'views/layouts')
-}))
-
-app.set('view engine', '.hbs')
 app.set('views', path.join(__dirname, 'views'))
 
 app.use(express.static('public'))
 
 app.get('/', (request, response) => {
-    response.render('home')
+    response.sendFile(path.join(__dirname, '/index.html'))
 })
 
 app.listen(port, (err) => {
