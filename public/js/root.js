24c2d3c73ff2fc7191ab538c7a315a4ba6dcefca
David Cruz <david@typokign.com>
2018-03-17 17:39:05 -0700

    Added MDL library and started building out main shop page with ngRepeated product cards

--- public/js/root.js
+++ public/js/root.js
@@ -2,4 +2,36 @@ var rootModule = angular.module('root', [])
 
 rootModule.controller("index", ["$scope", function ($scope) {
     $scope.message="Hello world"
+    $scope.itemsInCart = 0
+    $scope.products = [
+    {
+        name: "Product 1",
+        description: "First product",
+        price: 99.99
+    },
+    {
+        name: "Product 2",
+        description: "Second product",
+        price: 9.99
+    },
+    {
+        name: "Product 3",
+        description: "Third product",
+        price: 9.99
+    },
+    {
+        name: "Product 4",
+        description: "Fourth product",
+        price: 99.99
+    },
+    {
+        name: "Product 5",
+        description: "Fifth product",
+        price: 9.99
+    },
+    {
+        name: "Product 6",
+        description: "Sixth product",
+        price: 9.99
+    }]
 }])
