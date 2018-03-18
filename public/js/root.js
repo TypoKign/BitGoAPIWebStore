23a1974786da6a7a18ee6a90baededdaff5507e2
David Cruz <david@typokign.com>
2018-03-17 18:22:15 -0700

    Added static currency conversions and styled the page a bit more

--- public/js/root.js
+++ public/js/root.js
@@ -1,9 +1,8 @@
 var rootModule = angular.module('root', [])
 
 rootModule.controller("index", ["$scope", function ($scope) {
-    $scope.message="Hello world"
-    $scope.itemsInCart = 0
-    $scope.products = [
+    $scope.itemsInCart = 0 // Shown as a badge on the shopping cart icon (top right)
+    $scope.products = [ // List of products to populate the product cards
     {
         name: "Product 1",
         description: "First product",
@@ -34,4 +33,58 @@ rootModule.controller("index", ["$scope", function ($scope) {
         description: "Sixth product",
         price: 9.99
     }]
+
+    $scope.currencies = [ // List of supported currencies
+    {
+        name: "bitcoin",
+        ticker: "btc",
+        icon: "/svg/btc.svg",
+        iconColor: "orange",
+        exchangeRate: 10000 // TODO: get from API
+    },
+    {
+        name: "ether",
+        ticker: "eth",
+        icon: "/svg/eth.svg",
+        iconColor: "purple",
+        exchangeRate: 900 // TODO: get from API
+    },
+    {
+        name: "ripple",
+        ticker: "xrp",
+        icon: "/svg/xrp.svg",
+        iconColor: "blue",
+        exchangeRate: 0.80 // TODO: get from API
+    },
+    {
+        name: "bitcoincash",
+        ticker: "bch",
+        icon: "/svg/bch.svg",
+        iconColor: "lightGreen",
+        exchangeRate: 1200 // TODO: get from API
+    },
+    {
+        name: "litecoin",
+        ticker: "ltc",
+        icon: "/svg/ltc.svg",
+        iconColor: "silver",
+        exchangeRate: 200 // TODO: get from API
+    }
+    ]
+
+    // Add to cart and snackbar logic
+    var snackbarDiv = document.querySelector("#add-to-cart-snackbar")
+    var decrementItemsInCart = function() { $scope.itemsInCart-- }
+    $scope.addToCart = function(itemName) {
+        $scope.itemsInCart++;
+
+        var snackBarData = {
+            message: "Added " + itemName + " to cart!",
+            timeout: 4000,
+            actionHandler: decrementItemsInCart,
+            actionText: "Undo"
+        }
+
+        snackbarDiv.MaterialSnackbar.showSnackbar(snackBarData)
+    }
 }])
