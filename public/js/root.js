d7d92d01e880d9c9e17f55d416261d8f8b95a69f
David Cruz <david@typokign.com>
2018-03-18 17:11:40 -0700

    TIL Angular Material is a thing. Redesigned the entire page with Angular Material. Colored the SVG icons for each currency

--- public/js/root.js
+++ public/js/root.js
@@ -1,90 +1,107 @@
-var rootModule = angular.module('root', [])
+var rootModule = angular.module('root', ['ngMaterial'])
 
-rootModule.controller("index", ["$scope", function ($scope) {
+rootModule.controller("index", function ($scope, $mdSidenav, $mdDialog, $mdToast) {
     $scope.itemsInCart = 0 // Shown as a badge on the shopping cart icon (top right)
     $scope.products = [ // List of products to populate the product cards
-    {
-        name: "Product 1",
-        description: "First product",
-        price: 99.99
-    },
-    {
-        name: "Product 2",
-        description: "Second product",
-        price: 9.99
-    },
-    {
-        name: "Product 3",
-        description: "Third product",
-        price: 9.99
-    },
-    {
-        name: "Product 4",
-        description: "Fourth product",
-        price: 99.99
-    },
-    {
-        name: "Product 5",
-        description: "Fifth product",
-        price: 9.99
-    },
-    {
-        name: "Product 6",
-        description: "Sixth product",
-        price: 9.99
-    }]
-
-    $scope.currencies = [ // List of supported currencies
-    {
-        name: "bitcoin",
-        ticker: "btc",
-        icon: "/svg/btc.svg",
-        iconColor: "orange",
-        exchangeRate: 10000 // TODO: get from API
-    },
-    {
-        name: "ether",
-        ticker: "eth",
-        icon: "/svg/eth.svg",
-        iconColor: "purple",
-        exchangeRate: 900 // TODO: get from API
-    },
-    {
-        name: "ripple",
-        ticker: "xrp",
-        icon: "/svg/xrp.svg",
-        iconColor: "blue",
-        exchangeRate: 0.80 // TODO: get from API
-    },
-    {
-        name: "bitcoincash",
-        ticker: "bch",
-        icon: "/svg/bch.svg",
-        iconColor: "lightGreen",
-        exchangeRate: 1200 // TODO: get from API
-    },
-    {
-        name: "litecoin",
-        ticker: "ltc",
-        icon: "/svg/ltc.svg",
-        iconColor: "silver",
-        exchangeRate: 200 // TODO: get from API
-    }
+        {
+            name: "Product 1",
+            description: "First product",
+            price: 99.99
+        },
+        {
+            name: "Product 2",
+            description: "Second product",
+            price: 9.99
+        },
+        {
+            name: "Product 3",
+            description: "Third product",
+            price: 9.99
+        },
+        {
+            name: "Product 4",
+            description: "Fourth product",
+            price: 99.99
+        },
+        {
+            name: "Product 5",
+            description: "Fifth product",
+            price: 9.99
+        },
+        {
+            name: "Product 6",
+            description: "Sixth product",
+            price: 999.99
+        }
     ]
 
-    // Add to cart and snackbar logic
-    var snackbarDiv = document.querySelector("#add-to-cart-snackbar")
-    var decrementItemsInCart = function() { $scope.itemsInCart-- }
-    $scope.addToCart = function(itemName) {
-        $scope.itemsInCart++;
-
-        var snackBarData = {
-            message: "Added " + itemName + " to cart!",
-            timeout: 4000,
-            actionHandler: decrementItemsInCart,
-            actionText: "Undo"
+    $scope.currencies = [ // List of supported currencies
+        {
+            name: "bitcoin",
+            ticker: "btc",
+            icon: "/svg/btc.svg",
+            iconColor: "orange",
+            exchangeRate: 10000 // TODO: get from API
+        },
+        {
+            name: "ether",
+            ticker: "eth",
+            icon: "/svg/eth.svg",
+            iconColor: "purple",
+            exchangeRate: 900 // TODO: get from API
+        },
+        {
+            name: "ripple",
+            ticker: "xrp",
+            icon: "/svg/xrp.svg",
+            iconColor: "blue",
+            exchangeRate: 0.80 // TODO: get from API
+        },
+        {
+            name: "bitcoincash",
+            ticker: "bch",
+            icon: "/svg/bch.svg",
+            iconColor: "lightGreen",
+            exchangeRate: 1200 // TODO: get from API
+        },
+        {
+            name: "litecoin",
+            ticker: "ltc",
+            icon: "/svg/ltc.svg",
+            iconColor: "silver",
+            exchangeRate: 200 // TODO: get from API
         }
+    ]
 
-        snackbarDiv.MaterialSnackbar.showSnackbar(snackBarData)
+    $scope.productCategories = [
+        "Category 1",
+        "Category 2",
+        "Category 3",
+        "Category 4"
+    ]
+
+    $scope.openSidenav = function() {
+        $mdSidenav('sidenav').toggle()
     }
-}])
+
+    $scope.showDialog = function($event, url) {
+        $mdDialog.show({
+            parent: angular.element(document.body),
+            targetEvent: $event,
+            templateUrl: url,
+            clickOutsideToClose: true
+        })
+    }
+
+    // Add to cart and snackbar logic
+    $scope.addToCart = function(productName) {
+        $mdToast.show($mdToast.simple().textContent(`Added ${productName} to cart!`))
+    }
+})
+
+// Set the color theme of Angular Material
+rootModule.config(function ($mdThemingProvider) {
+    $mdThemingProvider.theme('default')
+        .primaryPalette('deep-purple')
+        .accentPalette('orange')
+})
